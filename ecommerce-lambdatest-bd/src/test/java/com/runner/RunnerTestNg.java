ebcd0e8d2342fe8714e83f0c52e3f02fbe2f8949
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-04-23 09:32:35 +0530

    Update RunnerTestNg.java

--- ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
+++ ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
 
     features = {
-        "src/test/resources/com/features/UserLogin.feature"
+        "src/test/resources/com/features/UserLogin.feature",
         "src/test/resources/com/features/UserRegistration.feature",
         "src/test/resources/com/features/ForgotPassword.feature",
         "src/test/resources/com/features/Search.feature",
