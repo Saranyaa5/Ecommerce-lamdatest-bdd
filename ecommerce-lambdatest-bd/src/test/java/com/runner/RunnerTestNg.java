43a6780878e16dc8780a30e4e878a7c742279a38
jeevika2407 <144089941+jeevika2407@users.noreply.github.com>
2025-04-24 15:09:53 +0530

    Update RunnerTestNg.java

--- ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
+++ ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
 
     features = {
-        "src/test/resources/com/features/UserLogin.feature",
+        "src/test/resources/com/features/Login.feature",
         "src/test/resources/com/features/Search.feature",
          "src/test/resources/com/features/Blog.feature",
     	 "src/test/resources/com/features/AffiliateAccount.feature"  
