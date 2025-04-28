03d7cfea302e2b680fb297a6275efd7187975a5e
Saranyaa5 <2k21cse128@kiot.ac.in>
2025-04-28 09:59:02 +0530

    pulled main

--- ecommerce-lambdatest-bd/src/test/java/com/pages/HomePageLocator.java
+++ ecommerce-lambdatest-bd/src/test/java/com/pages/HomePageLocator.java
@@ -8,7 +8,10 @@ public class HomePageLocator {
 	@FindBy(css="#entry_217964 a:first-of-type")
 	public WebElement blk1Shopnow;
 	
-	@FindBy(css="#entry_217967 > a")
+//	@FindBy(css="#entry_217967 > a")
+//	@FindBy(css="#entry_217967")
+//	@FindBy(css="#entry_217967 > a > div > img")
+	@FindBy(xpath="//*[@id='entry_217967']/a")
 	public WebElement blk2Shopnow;
 	
 	@FindBy(css="#entry_216815 > div : first-of-type")
