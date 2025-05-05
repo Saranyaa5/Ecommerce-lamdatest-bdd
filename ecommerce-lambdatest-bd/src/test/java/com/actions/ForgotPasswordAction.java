543696b80018b4e1f3dac7824d7247670b078efc
Soundaranarayanan <2k21eee65@kiot.ac.in>
2025-05-05 21:20:26 +0530

    Replaced single data table with scenario outline in forgot password feature for better data-driven testing.

--- ecommerce-lambdatest-bd/src/test/java/com/actions/ForgotPasswordAction.java
+++ ecommerce-lambdatest-bd/src/test/java/com/actions/ForgotPasswordAction.java
@@ -1,39 +1,3 @@
-//
-//package com.actions;
-//
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.testng.Assert;
-//import com.pages.ForgotPasswordLocator;
-//import com.utilities.HelperClass;
-//
-//public class ForgotPasswordAction {
-//
-//    ForgotPasswordLocator forgotPage = new ForgotPasswordLocator();
-//
-//    
-//  
-//
-//    public void clickForgotPassword() {
-//        forgotPage.forgotPasswordLink.click();
-//    }
-//
-//    public void enterEmail(String email) {
-//        forgotPage.emailInput.clear();
-//        forgotPage.emailInput.sendKeys(email);
-//    }
-//
-//    public void clickContinueButton() {
-//        forgotPage.continueButton.click();
-//    }
-//
-//    public String verifySuccessMessage() {
-//return forgotPage.successAlert.getText();
-//    }
-//
-//    public String verifyErrorMessage() {
-//       return forgotPage.errorAlert.getText();
-//    }
-//}
 package com.actions;
 
 import java.time.Duration;
