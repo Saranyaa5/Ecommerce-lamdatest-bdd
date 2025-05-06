9809623a91c1c259c0278c599f625759ec126f53
Soundaranarayanan <2k21eee65@kiot.ac.in>
2025-05-06 12:33:08 +0530

    changes in runner file

--- ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
+++ ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
@@ -7,12 +7,14 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
 
     features = {
-      "src/test/resources/com/features/ForgotPassword.feature",
+     "src/test/resources/com/features/ForgotPassword.feature",
       "src/test/resources/com/features/AddToCart.feature",
+      "src/test/resources/com/features/UserAccountManagement.feature",
       "src/test/resources/com/features/ProductReview.feature",
       "src/test/resources/com/features/EstimateShippingStatus.feature",
       "src/test/resources/com/features/FAQ.feature",
       "src/test/resources/com/features/Verify_product_url.feature",
+
     },
     glue = {"com.definitions"},
     plugin = {
