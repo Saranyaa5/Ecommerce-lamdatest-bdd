6bc6d8faa906b65533376df9f30e96b8bbfb8336
Soundaranarayanan <141643132+Soundaranarayanan@users.noreply.github.com>
2025-07-28 13:07:34 +0530

    Update RunnerTestNg.java

--- ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
+++ ecommerce-lambdatest-bd/src/test/java/com/runner/RunnerTestNg.java
@@ -10,20 +10,20 @@ import org.testng.annotations.DataProvider;
     features = {
 
 
-    		 // "src/test/resources/com/features/ForgotPassword.feature",
+    		  "src/test/resources/com/features/ForgotPassword.feature",
     	     
-    	  //     "src/test/resources/com/features/AddToCart.feature",
+    	       "src/test/resources/com/features/AddToCart.feature",
     	      "src/test/resources/com/features/UserAccountManagement.feature",
     	
-    	  //      "src/test/resources/com/features/ProductReview.feature",
+    	        "src/test/resources/com/features/ProductReview.feature",
     	     
-    	  //     "src/test/resources/com/features/EstimateShippingStatus.feature",
-    	  //     "src/test/resources/com/features/FAQ.feature",
+    	       "src/test/resources/com/features/EstimateShippingStatus.feature",
+    	       "src/test/resources/com/features/FAQ.feature",
  
-    	  //     "src/test/resources/com/features/Verify_product_url.feature",
+    	       "src/test/resources/com/features/Verify_product_url.feature",
     	     
-       //      "src/test/resources/com/features/ShoppingCartTableVerification.feature",
-       //      "src/test/resources/com/features/linkchecker.feature",
+             "src/test/resources/com/features/ShoppingCartTableVerification.feature",
+             "src/test/resources/com/features/linkchecker.feature",
 	    
 
     },
